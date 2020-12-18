34043e85d1618942acc570e5ca4827ae18785bb8
Kevin Long <kevin.uu.long@gmail.com>
2020-12-18 14:22:47 -0500

    Fix Path to Questions

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const fs = require('fs');
 const { fork } = require('child_process');
 
 // Check to see if 'questions.json' exists, if not it runs the generator
-if (!fs.existsSync("questions.json")) {
+if (!fs.existsSync("questions/questions.json")) {
     fork('generator.js');
 }
